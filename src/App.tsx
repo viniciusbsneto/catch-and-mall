ee93519b6e64d001efd3741d3ef34b6f95568758
Vinícius Neto <viniciusbsneto@gmail.com>
2020-07-26 13:50:18 -0300

    chore: create-react-app and home page

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Routes from './routes';
+import { BrowserRouter } from 'react-router-dom';
+
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>
+);
+
+export default App;
