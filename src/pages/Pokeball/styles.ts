4ce0c9fbb228a6f22ebc4941f5fc96b702c65da1
Vinícius Neto <viniciusbsneto@gmail.com>
2020-08-12 00:33:27 -0300

    chore: Add Pokeball page responsiveness

--- src/pages/Pokeball/styles.ts
+++ src/pages/Pokeball/styles.ts
@@ -9,6 +9,10 @@ export const Content = styled.main`
 
 export const PokeballWrapper = styled.div`
   display: flex;
+
+  @media (max-width: 1096px) {
+    flex-direction: column;
+  }
 `;
 
 export const PokemonTable = styled.table`
@@ -19,51 +23,80 @@ export const PokemonTable = styled.table`
   margin-right: 120px;
   margin-top: 32px;
 
-  thead th {
-    color: #999;
-    text-align: left;
-    padding: 12px;
-  }
+  thead {
+    th {
+      color: #999;
+      text-align: left;
+      padding: 12px;
+    }
 
-  tbody td {
-    padding: 12px;
-    border-bottom: 1px solid #eee;
-  }
-
-  img {
-    height: 100px;
-  }
-
-  strong {
-    color: #333;
-    display: block;
-    text-transform: capitalize;
-  }
-
-  span {
-    display: block;
-    margin-top: 5px;
-    font-size: 18px;
-    font-weight: bold;
-  }
-
-  div {
-    display: flex;
-    align-items: center;
-
-    input {
-      border: 1px solid #ddd;
-      border-radius: 4px;
-      color: #666;
-      padding: 6px;
-      width: 50px;
+    @media (max-width: 680px) {
+      display: none;
     }
   }
 
-  button {
-    background: none;
-    border: 0;
-    padding: 6px;
+  tbody {
+    tr {
+      @media (max-width: 680px) {
+        display: flex;
+        flex-direction: column;
+        border-bottom: 1px solid #eee;
+      }
+    }
+
+    td {
+      padding: 12px;
+      border-bottom: 1px solid #eee;
+
+      img {
+        height: 100px;
+      }
+
+      strong {
+        color: #333;
+        display: block;
+        text-transform: capitalize;
+      }
+
+      span {
+        display: block;
+        margin-top: 5px;
+        font-size: 18px;
+        font-weight: bold;
+
+        @media (max-width: 680px) {
+          display: none;
+        }
+      }
+
+      div {
+        display: flex;
+        align-items: center;
+
+        input {
+          border: 1px solid #ddd;
+          border-radius: 4px;
+          color: #666;
+          padding: 6px;
+          width: 50px;
+        }
+      }
+
+      button {
+        background: none;
+        border: 0;
+        padding: 6px;
+      }
+
+      @media (max-width: 680px) {
+        border: none;
+        align-self: center;
+      }
+    }
+  }
+
+  @media (max-width: 1096px) {
+    width: 100%;
   }
 `;
 
@@ -104,6 +137,15 @@ export const PokeballSummary = styled.aside`
 
   hr {
     border-bottom: 1px solid #eee;
+  }
+
+  @media (max-width: 1132px) {
+    height: 330px;
+  }
+
+  @media (max-width: 1096px) {
+    margin-top: 16px;
+    width: 100%;
   }
 `;
 
